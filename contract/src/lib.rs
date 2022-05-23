4a4bbab1f5a0d036f6465f2f29fc0fd846ccd0f6
Verkhohliad <ihor.verkhohliad@gmail.com>
2022-05-23 16:08:07 +0300

    [FEAT] implement crowfund models, contract functions

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -11,48 +11,83 @@
  *
  */
 
+mod models;
+mod utils;
+use crate::{
+    models::{Crowdfund, Donation},
+    utils::{assert_self, assert_single_promise_success, AccountId, ONE_NEAR},
+};
+
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, setup_alloc};
-use near_sdk::collections::LookupMap;
+#[allow(unused_imports)]
+use near_sdk::{env, near_bindgen, PromiseIndex};
+near_sdk::setup_alloc!();
 
-setup_alloc!();
-
-// Structs in Rust are similar to other languages, and may include impl keyword as shown below
-// Note: the names of the structs are not important when calling the smart contract, but the function names are
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Welcome {
-    records: LookupMap<String, String>,
-}
-
-impl Default for Welcome {
-  fn default() -> Self {
-    Self {
-      records: LookupMap::new(b"a".to_vec()),
-    }
-  }
+#[derive(Clone, Default, BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    owner: AccountId,
+    crowdfunds: Vec<Crowdfund>,
+    donations: Vec<Donation>,
 }
 
 #[near_bindgen]
-impl Welcome {
-    pub fn set_greeting(&mut self, message: String) {
-        let account_id = env::signer_account_id();
+impl Contract {
+    #[init]
+    pub fn init(owner: AccountId) -> Self {
+        let crowdfunds: Vec<Crowdfund> = Vec::new();
+        let donations: Vec<Donation> = Vec::new();
 
-        // Use env::log to record logs permanently to the blockchain!
-        env::log(format!("Saving greeting '{}' for account '{}'", message, account_id,).as_bytes());
-
-        self.records.insert(&account_id, &message);
-    }
-
-    // `match` is similar to `switch` in other languages; here we use it to default to "Hello" if
-    // self.records.get(&account_id) is not yet defined.
-    // Learn more: https://doc.rust-lang.org/book/ch06-02-match.html#matching-with-optiont
-    pub fn get_greeting(&self, account_id: String) -> String {
-        match self.records.get(&account_id) {
-            Some(greeting) => greeting,
-            None => "Hello".to_string(),
+        Contract {
+            owner,
+            crowdfunds,
+            donations,
         }
+    }
+
+    pub fn add_crowdfund(&mut self, title: String, donate: u128, description: String) {
+        let id = self.crowdfunds.len() as i32;
+        self.crowdfunds
+            .push(Crowdfund::new(id, title, donate, description));
+        env::log("Added a new crowdfund".as_bytes());
+    }
+
+    pub fn list_crowdfunds(&self) -> Vec<Crowdfund> {
+        assert_self();
+        let crowdfunds = &self.crowdfunds;
+        return crowdfunds.to_vec();
+    }
+
+    pub fn add_vote(&mut self, id: usize) {
+        let crowdfund: &mut Crowdfund = self.crowdfunds.get_mut(id).unwrap();
+        let voter = env::predecessor_account_id();
+        crowdfund.total_votes += 1;
+        env::log("vote submitted succesfully".as_bytes());
+        crowdfund.votes.push(voter);
+    }
+
+    // maybe not need to pass amount, just use env::attached_deposit instead
+    pub fn add_donation(&mut self, id: usize, amount: u128) {
+        let transfer_amount: u128 = ONE_NEAR * amount;
+        let crowdfund: &mut Crowdfund = self.crowdfunds.get_mut(id).unwrap();
+        crowdfund.total_donations = crowdfund.total_donations + transfer_amount;
+        self.donations.push(Donation::new());
+
+        println!("transfer_amount: {}", transfer_amount);
+        println!("amount: {}", amount);
+        println!("attached_deposit: {}", env::attached_deposit());
+        near_sdk::Promise::new(env::predecessor_account_id()).transfer(transfer_amount);
+        env::log("You have donated succesfully".as_bytes());
+    }
+
+    pub fn crowdfund_count(&mut self) -> usize {
+        return self.crowdfunds.len();
+    }
+
+    pub fn get_total_donations(&mut self, id: usize) -> u128 {
+        let crowdfund: &mut Crowdfund = self.crowdfunds.get_mut(id).unwrap();
+        return crowdfund.total_donations;
     }
 }
 
